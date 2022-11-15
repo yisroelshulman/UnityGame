040bdcfdd5acbbb9555f13b7aad0be0bd4486616
yisroel <git@yisroelshulman.com>
2022-11-15 00:43:27 -0500

    First Commit

---
+++ game The Bell Tower Beacon/Assets/ButtonBehavior.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+///using UnityEngine.UI;
+
+public class ButtonBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// needs info
+    public void Rules()
+    {
+        
+    }
+
+    /// Needs funbctionality
+    public void Buttons()
+    {
+
+    }
+
+    /// needs info
+    public void Tutorial()
+    {
+        
+    }
+
+    public void SelectionMenu()
+    {
+        SceneManager.LoadScene("SelectionMenu");
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Campus");
+    }
+
+}
